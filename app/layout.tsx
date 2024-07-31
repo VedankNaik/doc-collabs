d3d993b9dc7fe564393cf34bd35e6056dee6068e
Vedank Naik <vedankn01@gmail.com>
2024-07-31 22:44:16 +0530

    CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,18 @@
-import "@/styles/globals.css";
-import { Inter as FontSans } from "next/font/google";
-
-import { cn } from "@/lib/utils";
 import React from "react";
+import { Inter as FontSans } from "next/font/google";
+import { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import "./globals.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "DoCollabs",
+  description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
   children,
@@ -16,14 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable
         )}
       >
-        ...
+        {children}
       </body>
     </html>
   );
