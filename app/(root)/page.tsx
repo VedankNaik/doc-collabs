1ab641a4a9eaf57e110324bb12cc0e7c3bb925d4
Vedank Naik <vedankn01@gmail.com>
2024-08-12 14:35:09 +0530

    Feat: Delete doc

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ import { redirect } from "next/navigation";
 import { getDocuments } from "@/lib/actions/room.actions";
 import Link from "next/link";
 import { dateConverter } from "@/lib/utils";
+import { DeleteModal } from "@/components/DeleteModal";
 
 const Home = async () => {
   const clerkUser = await currentUser();
@@ -60,7 +61,7 @@ const Home = async () => {
                       </p>
                     </div>
                   </Link>
-                  {/* <DeleteModal roomId={id} /> */}
+                  <DeleteModal roomId={id} />
                 </li>
               ))}
             </ul>
